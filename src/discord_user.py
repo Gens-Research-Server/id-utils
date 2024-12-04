71e35d9ee2c051b51c78f66f5e9684c42841c0b4
Shell1010 <amin.dev03@gmail.com>
2024-12-04 18:01:44 +0000

    figuring out middle part new tokens

--- src/discord_user.py
+++ src/discord_user.py
@@ -24,15 +24,21 @@ class DiscordUser:
             datetime.fromtimestamp(self.second_timestamp) if self.second_timestamp else None
         )
 
+        self.new_second_timestamp: Optional[float] = self._decode_new_second_to_timestamp()
+        self.new_second_date: Optional[datetime] = (
+            datetime.fromtimestamp(self.new_second_timestamp) if self.new_second_timestamp else None
+        )
+
     @classmethod
     def from_token(cls, token: str) -> DiscordUser:
         first, second, third = token.split(".")
         id = int(base64.b64decode(first + "=="))
         return DiscordUser(id, first, second, third)
 
+    @property
     def base64_token(self) -> str:
         """Generates a Base64 token from the user's ID."""
-        return base64.b64encode(self.id.to_bytes((self.id.bit_length() + 7) // 8, byteorder="big")).decode().removesuffix("==")
+        return base64.b64encode(str(self.id).encode()).decode().removesuffix("==")
 
     def _decode_second_to_timestamp(self) -> Optional[float]:
         """Helper function to decode the second part of the token."""
@@ -41,6 +47,17 @@ class DiscordUser:
         try:
             decoded = base64.b64decode(self.second + "==")
             return int.from_bytes(decoded, byteorder="big")
+        except Exception as e:
+            print(f"Error decoding second part: {e}")
+            return None
+
+    def _decode_new_second_to_timestamp(self) -> Optional[float]:
+        """Helper function to decode the second part of the token."""
+        if not self.second:
+            return None
+        try:
+            decoded = base64.b64decode(self.second + "==")
+            return (int.from_bytes(decoded, byteorder="big") + self.epoch) / 1000
         except Exception as e:
             print(f"Error decoding second part: {e}")
             return None
